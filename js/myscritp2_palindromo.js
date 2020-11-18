14e9026f97be0d93f97112977c30a57fc982f129
Emilo <emilio.furnari87@gmail.com>
2020-11-18 15:44:05 +0100

    parola girata

--- js/myscritp2_palindromo.js
+++ js/myscritp2_palindromo.js
@@ -3,13 +3,16 @@
 // Creare una funzione per capire se la parola inserita è palindroma.
 
 
-var userWord = prompt("scrivi una parola")
+// var userWord = isPalindrome(prompt("Scrivi una parola"));
+var userWord = prompt("Scrivi qua una parola");
 console.log(userWord);
 
+var conversione = revString(userWord);
+console.log(conversione);
 
-function isPalindrome (word) {
-
-  for (var i = userWord.lenght; i > 0; i-- ) {
-    console.log(i);
-  }
+function revString(str) {
+var splString = str.split("");
+var revArray = splString.reverse();
+return revArray.join("");
+console.log(revArray);
 }
