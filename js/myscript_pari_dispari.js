02bec7e1cf0db50701766c5431e62f9e992ffe28
Emilo <emilio.furnari87@gmail.com>
2020-11-18 16:44:08 +0100

    finito secondo esercizio

--- js/myscript_pari_dispari.js
+++ js/myscript_pari_dispari.js
@@ -5,3 +5,32 @@
 // Sommiamo i due numeri.
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
+
+
+// SCELTE UTENTE
+var pariOdispari = prompt("scrivi pari o dispari")
+console.log("io ho scelto ", pariOdispari);
+var userNumber = parseInt(prompt("ora inserisci un numero da 1 a 5"));
+console.log("il mio numero ", userNumber);
+// SCELTE PC
+var pcNumber = Math.floor(Math.random() * 5) + 1;
+console.log("il numero del pc ", pcNumber);
+
+
+// SOMMA
+var somma = userNumber + pcNumber;
+console.log(somma);
+
+if (somma % 2 == 0 && pariOdispari == "pari") {
+  alert("hai vinto")
+  console.log("hai vinto");
+} else if (somma % 2 == 0 && pariOdispari == "dispari") {
+  alert("hai perso")
+  console.log("hai perso");
+} else if (somma % 2 != 0 && pariOdispari == "dispari") {
+  alert("hai vinto")
+  console.log("hai vinto");
+} else if (somma % 2 != 0 && pariOdispari == "pari") {
+  alert("hai perso")
+  console.log("hai perso");
+}
