8403aa6ae64643a1849695545a275a5f3fbeef44
Emilo <emilio.furnari87@gmail.com>
2020-11-18 14:44:31 +0100

    secondo push

--- js/myscritp2_palindromo.js
+++ js/myscritp2_palindromo.js
@@ -1,3 +1,15 @@
 // Palindroma
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma.
+
+
+var userWord = prompt("scrivi una parola")
+console.log(userWord);
+
+
+function isPalindrome (word) {
+
+  for (var i = userWord.lenght; i > 0; i-- ) {
+    console.log(i);
+  }
+}
